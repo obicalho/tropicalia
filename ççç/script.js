0340d2e13061dffba32c3e82bda254b51512dfbf
seu espedito <142508031+obicalho@users.noreply.github.com>
2025-04-02 21:00:23 -0300

    Add files via upload

--- ççç/script.js
+++ ççç/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded',function(){
 
 const botaoDeAcessesibilidade=document.getElementById('bota-acessibilidade');    
+const opcoesDeAcessbilidade=docuent.getElementsById('opcoes-acessibilidade');
+
+botaoDeAcessesibilidade.addEventListener('click',function(){
+    botaoDeAcessesibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+
+    const botaoSelecionado=botaoDeAcessesibilidade.getAttribute('aria-expanded')=== 'true';
 })
+
+
+
+})
+
